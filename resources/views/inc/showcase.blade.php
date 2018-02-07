3427f5266f3a4950e083179adfe0cc432c81668a
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-02-07 15:43:54 +0300

    Adds template

---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,8 @@
+<div class="jumbotron">
+    <div class="container">
+        <h1>Welcome to our site</h1>
+        <p class="lead">
+            Welcome to my new Laravel web site.
+        </p>
+    </div>
+</div>

21b3d4adf621f2c3e978ef9930eaf1423e12938f
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-02-07 16:53:14 +0300

    Adds saving message to database

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessagesController extends Controller
 {
@@ -15,6 +16,13 @@ class MessagesController extends Controller
             ]
 
         );
-        return 'OK';
+
+        $message = new Message();
+        $message->name = $request->input('name');
+        $message->email = $request->input('email');
+        $message->message = $request->input('message');
+        $message->save();
+
+        return redirect('/')->with(['success' => 'Message Sent']);
     }
 }

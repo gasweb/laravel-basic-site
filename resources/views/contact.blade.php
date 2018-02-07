d2a801a48c765e95333c81259c50decff5a35da8
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-02-07 16:14:38 +0300

    Adds form with collective

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,4 +3,21 @@
 @section('content')
 <h1>Contact</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem beatae cupiditate deserunt dolorum eaque enim error excepturi exercitationem ipsam necessitatibus nobis omnis placeat quidem quo sapiente, sed sint tenetur ullam.</p>
+{!! Form::open(['url' => 'contact/submit']) !!}
+<div class="form-group">
+    {{ Form::label('name', 'Name') }}
+    {{ Form::text('name', '', ['placeholder' => 'Enter your name', 'class' => 'form-control', 'autocomplete' => 'off']) }}
+</div>
+<div class="form-group">
+    {{ Form::label('email', 'E-Mail Address') }}
+    {{ Form::text('email', '', ['placeholder' => 'Enter your email', 'class' => 'form-control', 'autocomplete' => 'off']) }}
+</div>
+<div class="form-group">
+    {{ Form::label('message', 'Message') }}
+    {{ Form::textarea('message', '', ['placeholder' => 'Enter message', 'class' => 'form-control', 'autocomplete' => 'off']) }}
+</div>
+<div>
+    {{ Form::submit('Submit', ['class' => 'btn btn-primary']) }}
+</div>
+{!! Form::close() !!}
 @endsection
